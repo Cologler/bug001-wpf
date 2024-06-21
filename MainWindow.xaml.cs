8ba02a1734fc8a470a4c9faa3a9119699fa9ed0a
Cologler <skyoflw@gmail.com>
2024-06-21 13:11:53 +0800

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Bug001;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void RunButton_Click(object sender, RoutedEventArgs e) => new MenuItem() { Header = string.Empty };
+}
